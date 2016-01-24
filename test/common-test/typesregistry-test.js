1645f44d26040687d395270af59759d2cdaf01f2
carrascogit <carrascogit@modeldd.org>
2016-01-24 19:04:18 +0100

    OK typesregistry-test

--- test/common-test/typesregistry-test.js
+++ test/common-test/typesregistry-test.js
@@ -40,21 +40,90 @@
 describe("TypesRegistry tests", function () {
 
     var aModule_TypesRegistrySvceFactory = ModuleFactory_TypesRegistrySvce();
+    /*
     console.log( "typeof aModule_TypesRegistrySvceFactory= " + typeof aModule_TypesRegistrySvceFactory);
+    */
 
     var aTypesRegistrySvce = aModule_TypesRegistrySvceFactory();
+    /*
     console.log( "typeof aTypesRegistrySvce= " + typeof aTypesRegistrySvce);
     console.log( "aTypesRegistrySvce keys = " + Object.keys( aTypesRegistrySvce));
+    */
 
 
-    it("Has type", function () {
+    it("Has module defined", function () {
+        expect( aTypesRegistrySvce._v_Module).not.toBeUndefined();
+    });
+
+    it("Has module not null", function () {
+        expect( aTypesRegistrySvce._v_Module).not.toBeNull( null);
+    });
+
+    it("Has module ModuleName typesregistry", function () {
+        expect( aTypesRegistrySvce._v_Module.ModuleName).toBe( "typesregistry");
+    });
+
+    it("Has module ModulePackages typesregistry", function () {
+        expect( aTypesRegistrySvce._v_Module.ModulePackages).toBe( "common");
+    });
+
+    it("Has module ModuleFullName common.typesregistry", function () {
+        expect( aTypesRegistrySvce._v_Module.ModuleFullName).toBe( "common/typesregistry");
+    });
+
+    it("Has module TypesRegistry_Prototype defined", function () {
+        expect( aTypesRegistrySvce._v_Module.TypesRegistry_Prototype).not.toBeUndefined();
+    });
+
+    it("Has module TypesRegistry_Prototype not null", function () {
+        expect( aTypesRegistrySvce._v_Module.TypesRegistry_Prototype).not.toBeNull( null);
+    });
+
+    it("Has module TypesRegistry_Constructor defined", function () {
+        expect( aTypesRegistrySvce._v_Module.TypesRegistry_Constructor).not.toBeUndefined();
+    });
+
+    it("Has module TypesRegistry_Constructor not null", function () {
+        expect( aTypesRegistrySvce._v_Module.TypesRegistry_Constructor).not.toBeNull( null);
+    });
+
+    it("Has module TypesRegistry_SuperPrototypeConstructor defined", function () {
+        expect( aTypesRegistrySvce._v_Module.TypesRegistry_SuperPrototypeConstructor).not.toBeUndefined();
+    });
+
+    it("Has module TypesRegistry_SuperPrototypeConstructor not null", function () {
+        expect( aTypesRegistrySvce._v_Module.TypesRegistry_SuperPrototypeConstructor).not.toBeNull( null);
+    });
+
+
+
+    it("Has _v_Prototype defined", function () {
+        expect( aTypesRegistrySvce._v_Prototype).not.toBeUndefined();
+    });
+
+    it("Has _v_Prototype module TypesRegistry_Prototype", function () {
+        expect( aTypesRegistrySvce._v_Prototype).toBe( aTypesRegistrySvce._v_Module.TypesRegistry_Prototype);
+    });
+
+    it("Has _v_Prototype_TypesRegistry defined", function () {
+        expect( aTypesRegistrySvce._v_Prototype_TypesRegistry).not.toBeUndefined();
+    });
+
+    it("Has _v_Prototype_TypesRegistry module TypesRegistry_Prototype", function () {
+        expect( aTypesRegistrySvce._v_Prototype).toBe( aTypesRegistrySvce._v_Module.TypesRegistry_Prototype);
+    });
+
+
+
+    it("Has _v_Type TypesRegistry", function () {
         expect( aTypesRegistrySvce._v_Type).toBe( "TypesRegistry");
     });
 
-
-    it("Has title", function () {
+    it("Has title TypesRegistry_DefaultName", function () {
         expect( aTypesRegistrySvce._v_Title).toBe( "TypesRegistry_DefaultName");
     });
+
+    
 });
 
 
