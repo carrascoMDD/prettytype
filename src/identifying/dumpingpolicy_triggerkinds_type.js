9c33a79d69a0563110e4d9bf472964917b112f55
carrascoMDD <carrascomdd@gmail.com>
2016-11-03 18:40:59 +0100

    OK_commoneventkinds_sets

--- src/identifying/dumpingpolicy_triggerkinds_type.js
+++ src/identifying/dumpingpolicy_triggerkinds_type.js
@@ -9,18 +9,18 @@
  Copyright 2014 2015 2016 Antonio Carrasco Valero
  Javascript for core modules including a base prototype and prototypes hierarchy, intended to be reused on the Browser as core for i.e. Angular Controllers and Services, as in the uiwire component. Licensed under EUPL  http://www.uiwire.org
 
-Licensed under the EUPL, Version 1.1 only (the "Licence");
-You may not use this work except in compliance with the
-Licence.
-You may obtain a copy of the Licence at:
-https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
-Unless required by applicable law or agreed to in
-writing, software distributed under the Licence is
-distributed on an "AS IS" basis,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-express or implied.
-See the Licence for the specific language governing
-permissions and limitations under the Licence.
+ Licensed under the EUPL, Version 1.1 only (the "Licence");
+ You may not use this work except in compliance with the
+ Licence.
+ You may obtain a copy of the Licence at:
+ https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+ Unless required by applicable law or agreed to in
+ writing, software distributed under the Licence is
+ distributed on an "AS IS" basis,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ express or implied.
+ See the Licence for the specific language governing
+ permissions and limitations under the Licence.
  {{License2}}
 
  {{Licensed1}}
@@ -581,6 +581,10 @@ function ModuleFactory_DumpingPolicyTriggerKindsType() {
                     for( var aRecordToDumpIdx=0; aRecordToDumpIdx < aNumRecordsToDump; aRecordToDumpIdx++) {
                         var aRecordToDump = someRecordsToDump[ aRecordToDumpIdx];
                         if( aRecordToDump) {
+
+                            if( !this.fMustDumpRecord( aRecordToDump)) {
+                                continue;
+                            }
 
                             var aLogString = aRecordToDump.fLogString();
                             if( !aLogString) {
