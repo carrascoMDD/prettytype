9e1c07a38e70cedda8efb55d43273e04fab23cba
carrascoMDD <carrascomdd@gmail.com>
2018-05-09 07:30:23 +0200

    Added README

---
+++ README.md
@@ -0,0 +1,2 @@
+# prettytype
+Javascript for core modules including a base prototype and prototypes hierarchy, intended to be reused on the Browser as core for i.e. Angular Controllers and Services, as in the uiwire component. Used i.e. in the SpringNut.org example. http://www.springnut.org/springnut
--- build/prettytype.js
+++ build/prettytype.js
@@ -2119,6 +2119,217 @@ if( ModuleFactory_OverriderType){}/* CQT */
 
 
 ;/*
+ * commoneventkinds.js
+ *
+ * Created @author Antonio Carrasco Valero 201610051556
+ *
+ *
+ ***************************************************************************
+
+ Copyright 2014 2015 2016 Antonio Carrasco Valero
+ Javascript for core modules including a base prototype and prototypes hierarchy, intended to be reused on the Browser as core for i.e. Angular Controllers and Services, as in the uiwire component. Licensed under EUPL  http://www.uiwire.org
+
+ Licensed under the EUPL, Version 1.1 only (the "Licence");
+ You may not use this work except in compliance with the
+ Licence.
+ You may obtain a copy of the Licence at:
+ https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+ Unless required by applicable law or agreed to in
+ writing, software distributed under the Licence is
+ distributed on an "AS IS" basis,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ express or implied.
+ See the Licence for the specific language governing
+ permissions and limitations under the Licence.
+ {{License2}}
+
+ {{Licensed1}}
+ {{Licensed2}}
+
+ ***************************************************************************
+ *
+ */
+
+
+
+
+
+function ModuleFactory_CommonEventKinds() {
+
+    'use strict';
+
+    return ( function( theSS_typesregistry){
+
+
+        var ModuleName     = "commoneventkinds";
+        var ModulePackages = "base";
+        var ModuleFullName = ModulePackages + "/" + ModuleName;
+
+
+
+        var aMod_definer = function() {
+
+
+            if( !( typeof FG_logModLoads == "undefined") && ( typeof FG_logModLoads == "function") && FG_logModLoads()) { FG_logModLoads(ModuleFullName);}
+
+
+
+
+
+
+            var pgInitWithModuleConstants = function( theToInit) {
+
+                if( !theToInit) {
+                    return;
+                }
+    
+                theToInit.EVENTKIND_NEWOBJECTCREATED        = "EVT_NEWOBJECTCREATED";
+                theToInit.EVENTKIND_NEWOBJECTFAILED         = "EVT_NEWOBJECTFAILED";
+
+
+                theToInit.EVENTKINDS_NOPROMISE_FAILURES = [
+                    theToInit.EVENTKIND_NEWOBJECTFAILED
+                ];
+
+
+
+                theToInit.EVENTKINDS_NOPROMISE_NOTFAILURES = [
+                    theToInit.EVENTKIND_NEWOBJECTCREATED
+                ];
+
+
+
+                theToInit.EVENTKINDS_NOPROMISE = [];
+                Array.prototype.push.apply( theToInit.EVENTKINDS_NOPROMISE, theToInit.EVENTKINDS_NOPROMISE_NOTFAILURES);
+                Array.prototype.push.apply( theToInit.EVENTKINDS_NOPROMISE, theToInit.EVENTKINDS_NOPROMISE_FAILURES);
+
+
+
+
+                theToInit.EVENTKINDS_FAILURES = [ ];
+                Array.prototype.push.apply( theToInit.EVENTKINDS_FAILURES, theToInit.EVENTKINDS_NOPROMISE_FAILURES);
+
+
+
+
+                theToInit.EVENTKINDS_NOTFAILURES = [ ];
+                Array.prototype.push.apply( theToInit.EVENTKINDS_NOTFAILURES, theToInit.EVENTKINDS_NOPROMISE_NOTFAILURES);
+
+
+
+
+
+
+
+                theToInit.EVENTKINDS = theToInit.EVENTKINDS_NOPROMISE.slice();
+
+
+
+                theToInit.EVENTKINDS_NOTFORCONSOLE_RESTRICTIVE = [
+                    theToInit.EVENTKIND_NEWOBJECTCREATED
+                ];
+
+
+
+                theToInit.EVENTKINDS_NOTFORCONSOLE_RESTRICTIVE_NOPROMISE = theToInit.EVENTKINDS_NOTFORCONSOLE_RESTRICTIVE.slice();
+                Array.prototype.push.apply( theToInit.EVENTKINDS_NOTFORCONSOLE_RESTRICTIVE_NOPROMISE, theToInit.EVENTKINDS_PROMISE_NOTFAILURES);
+
+                theToInit.EVENTKINDS_NOTFORCONSOLE_NOPROMISE = theToInit.EVENTKINDS_NOPROMISE_NOTFAILURES.slice();
+
+
+                theToInit.EVENTKINDS_NOTFORCONSOLE_NONE = [];
+                theToInit.EVENTKINDS_NOTFORCONSOLE_ALL = theToInit.EVENTKINDS.slice();
+
+                theToInit.EVENTKINDS_NOTFORCONSOLE_DEFAULT = theToInit.EVENTKINDS_NOTFORCONSOLE_RESTRICTIVE_NOPROMISE.slice();
+
+                theToInit.EVENTKINDS_NOTFORCONSOLE_DEFAULT = theToInit.EVENTKINDS_NOTFORCONSOLE_NONE;
+
+                theToInit.EVENTKINDS_NOTFORCONSOLE_DEFAULT = theToInit.EVENTKINDS_NOTFORCONSOLE_NOPROMISE;
+
+
+
+
+
+
+                theToInit.EVENTKINDS_ERRORS = [
+                    theToInit.EVENTKIND_NEWOBJECTFAILED
+                ];
+
+
+                theToInit.EVENTKINDS_TRIGGERDUMP_ERRORS  = theToInit.EVENTKINDS_ERRORS.slice();
+
+                theToInit.EVENTKINDS_TRIGGERDUMP_ALL     = theToInit.EVENTKINDS.slice();
+
+                theToInit.EVENTKINDS_TRIGGERDUMP_DEFAULT = theToInit.EVENTKINDS_TRIGGERDUMP_ERRORS;
+
+
+
+            };
+
+
+
+            var ModuleConstants = {};
+            pgInitWithModuleConstants( ModuleConstants);
+
+
+
+
+            var pgInitFromModuleConstants = function( theToInit) {
+                if( !theToInit) {
+                    return;
+                }
+
+                for( var aGlobalName in ModuleConstants) {
+                    if( ModuleConstants.hasOwnProperty( aGlobalName)) {
+                        theToInit[ aGlobalName] = ModuleConstants[ aGlobalName];
+                    }
+                }
+            };
+
+
+
+            var aModule = { };
+            pgInitFromModuleConstants( aModule);
+            aModule.ModuleName      = ModuleName;
+            aModule.ModulePackages  = ModulePackages;
+            aModule.ModuleFullName  = ModuleFullName;
+            aModule.ModuleConstants = ModuleConstants;
+            aModule.pgInitFromModuleConstants = pgInitFromModuleConstants;
+
+
+
+            return aModule;
+        };
+
+
+
+
+
+
+        var anExistingModule = theSS_typesregistry.fRegisteredModule( ModuleFullName);
+        if( !anExistingModule) {
+
+            var aModule = aMod_definer();
+            anExistingModule = aModule;
+
+            theSS_typesregistry.fRegisterModule( ModuleFullName, aModule);
+        }
+
+
+
+        return anExistingModule;
+
+
+    });
+}
+
+
+if( ModuleFactory_CommonEventKinds){}/* CQT */
+
+
+
+
+;/*
  * common_type.js
  *
  * Created @author Antonio Carrasco Valero 201410030426
