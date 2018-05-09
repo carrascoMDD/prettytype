4ad522ea08fceef425ba437bd5f7577c08b6c000
carrascoMDD <carrascomdd@gmail.com>
2018-05-09 02:33:42 +0200

    Version v0.1.21 adapted for node v8.11.1

--- install.sh
+++ install.sh
@@ -2,12 +2,10 @@
 
 # Must have node & npm installed (npw now ships with npm included)
 
-
 # installs from public repositories into node_modules used by the build process as specified in package.json
 npm install
 
-
 # installs from public repositories into bower_components the ones specified in bower.json 
-bower install
+npm run bower install
 
 
